88ad05f04d88ce6a57deb0a95402e6e1c93a55f5
RedFu01 <konradfuger@googlemail.com>
2016-06-21 11:30:59 +0200

    shoot travis

--- src/modules/search/__specs__/SearchView.spec.js
+++ src/modules/search/__specs__/SearchView.spec.js
@@ -6,7 +6,7 @@ import { expect } from 'chai';
 describe('<SearchView />', () => {
   it('should render the SearchView', () => {
     const wrapper = shallow(<SearchView />);
-    expect(wrapper.length).to.equal(1);
+    expect(wrapper.length).to.equal(1);  
     //expect(wrapper.contains(<Text>I wonder if there will be any problems...</Text>)).to.equal(true);
   });
 });
